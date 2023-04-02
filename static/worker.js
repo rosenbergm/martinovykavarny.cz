7e023851799f68ce701d2a22691baaae73642bee
Martin Rosenberg <nitram.rosenberg@gmail.com>
2023-04-02 15:46:57 +0200

    ⚗️ More PWA shenanigans

--- static/worker.js
+++ static/worker.js
@@ -1,5 +1,10 @@
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+const OFFLINE_URL = '/';
+
+const filesToCache = [
+  "/",
+  '/static'
+]
 
 self.addEventListener('install', function (event) {
   console.log('[ServiceWorker] Install');
@@ -8,7 +13,8 @@ self.addEventListener('install', function (event) {
     const cache = await caches.open(CACHE_NAME);
     // Setting {cache: 'reload'} in the new request will ensure that the response
     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+    // await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+    await cache.addAll(filesToCache)
   })());
 
   self.skipWaiting();
